a980336a803a34f0dcba658dfb3ededa5dc9d157
deliciousqoo <66346619+deliciousqoo@users.noreply.github.com>
2021-11-15 00:23:16 +0900

    Update fibo.c

--- shinjihan/fibo.c
+++ shinjihan/fibo.c
@@ -1,37 +1,33 @@
-/*#include <stdio.h>
-
-int count0 = 0;
-int count1 = 0;
-
-int fibonacci(int n) {
-	if (n == 0) {
-		count0++;
-		return 0;
-	}
-	else if (n == 1) {
-		count1++;
-		return 1;
-	}
-	else
-	{
-		return fibonacci(n - 1) + fibonacci(n - 2);
-	}
-}
+#include <stdio.h>
 
 int main(void) {
 
-	int rpt;
-	scanf(" %d", &rpt);
+	int count;
+	scanf(" %d", &count);
 
+	int fibo[40];
+
+	fibo[0] = 1;
+	fibo[1] = 1; 
+	for (int i = 2; i < 40; i++)	{
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+	
 	int num = 0;
-	for (int i = 0; i < rpt; i++) {
-		count0 = 0;
-		count1 = 0;
+	for (int i = 0; i < count; i++) {
 		scanf(" %d", &num);
-		fibonacci(num);
-
-		printf("%d %d\n", count0, count1);
+		
+		if (num == 0) {
+			printf("0 1\n");
+		}
+		else if (num == 1) {
+			printf("1 0\n");
+		}
+		else
+		{
+			printf("%d %d\n", fibo[num - 2], fibo[num - 1]);
+		}
 	}
 
 	return 0;
-}*/
+}
